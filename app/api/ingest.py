7cf450303aa47d1f85cf3681cb0d5c00d26d4b9b
Shashwat Jain <kcapshashwat@gmail.com>
2025-10-17 12:20:05 +0530

    Added proper error handling, and updated the output format for better understand of the Tasks. This endpoint is completed!

--- app/api/ingest.py
+++ app/api/ingest.py
@@ -25,15 +25,32 @@ def ingest_url(request: IngestRequest):
             db.query(URLMetadata).filter(URLMetadata.url == str(request.url)).first()
         )
         if existing:
-            raise HTTPException(status_code=400, detail="URL already submitted")
+            if existing.status == IngestionStatus.failed:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"URL already processed and was Failed. Please reprocess the URL {existing.url}.",
+                )
 
-        metadata = URLMetadata(url=str(request.url), status=IngestionStatus.pending)
-        db.add(metadata)
-        db.commit()
-        db.refresh(metadata)
+            if existing.status in [IngestionStatus.pending, IngestionStatus.processing]:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"URL already queued with current status: {existing.status}. Please wait for it to complete.",
+                )
+
+            if existing.status == IngestionStatus.completed:
+                raise HTTPException(
+                    status_code=400,
+                    detail="URL has been proccessed successfully, You can query it.",
+                )
+
+        else:
+            metadata = URLMetadata(url=str(request.url), status=IngestionStatus.pending)
+            db.add(metadata)
+            db.commit()
+            db.refresh(metadata)
 
         try:
-            q.enqueue("app.worker.worker.process_url", metadata.id)
+            job = q.enqueue("app.worker.worker.process_url", metadata.id)
         except Exception as e:
             # If queue fails, mark as failed so user knows
             metadata.status = IngestionStatus.failed
@@ -44,6 +61,7 @@ def ingest_url(request: IngestRequest):
         return {
             "message": "URL submitted successfully",
             "url_id": metadata.id,
+            "job_id": job.id,
             "status": metadata.status,
         }
     finally:
